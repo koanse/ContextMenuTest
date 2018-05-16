6557242b3b1b66892b31c961645fab8a905de950
koanse <ant.kondaurov@gmail.com>
2018-05-17 02:15:39 +0300

    Initial commit.

---
+++ ContextMenuTest/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ContextMenuTest
+{
+    public partial class Form1 : Form
+    {
+        AdvancedListView advancedListView;
+        public Form1()
+        {
+            InitializeComponent();
+            advancedListView = new AdvancedListView();
+            advancedListView.Parent = this;
+            advancedListView.Dock = DockStyle.Fill;
+            advancedListView.View = View.Details;
+            advancedListView.Columns.Add("Column1", 100, HorizontalAlignment.Left);
+            advancedListView.Items.Add(new ListViewItem("listViewItem"));
+        }
+    }
+
+    public class AdvancedListView : ListView
+    {
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            if (e.X < 120) MessageBox.Show("Item selected");
+            else MessageBox.Show("ContextMenu");
+            base.OnMouseUp(e);
+        }
+    }
+}
---
+++ ContextMenuTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ContextMenuTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ЛИПЕЦК")]
+[assembly: AssemblyProduct("ContextMenuTest")]
+[assembly: AssemblyCopyright("Copyright © ЛИПЕЦК 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2514c6a-9bff-4cc2-b410-41a51936cac7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+
